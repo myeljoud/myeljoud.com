2d8fe22fafaa568dc48027a60dd3964a9da68ed5
Mohamed Yahye El Joud <yahyajoud@gmail.com>
2023-04-19 05:33:30 +0000

    Updating "soon" text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen  items-center justify-center bg-black">
-      <h1 className="font-serif text-2xl text-white">Soon</h1>
+      <h1 className="font-sans text-white select-none">Soon</h1>
     </main>
-  )
+  );
 }
